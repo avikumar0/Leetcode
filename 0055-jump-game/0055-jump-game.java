fd20deff64eab6742b4af7e14c384bcfa5b47f9c
Avinash Kumar <114355501+avikumar0@users.noreply.github.com>
2023-10-02 11:39:35 +0530

    Time: 328 ms (9.61%), Space: 44.5 MB (23.88%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n=nums.length-1;
-        for(int i=nums.length-2;i>=0;i--){
-            if(i+nums[i]>=n){
-                n=i;
+        int n = nums.length;
+        boolean[] canReach = new boolean[n]; // Create a boolean array to track reachability.
+        canReach[0] = true; // You can reach the first position.
+
+        // Iterate through the array and update the reachability array.
+        for (int i = 0; i < n; i++) {
+            if (canReach[i]) {
+                int maxJump = nums[i];
+                for (int j = 1; j <= maxJump && i + j < n; j++) {
+                    canReach[i + j] = true;
+                }
             }
         }
-        return n==0;
+
+        return canReach[n - 1]; // Check if you can reach the last position.
     }
 }
