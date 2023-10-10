9d6ba5b56524294074a0ed8946d7aa899b70e10a
Avinash Kumar <114355501+avikumar0@users.noreply.github.com>
2023-10-10 20:58:03 +0530

    Time: 1 ms (98.67%), Space: 40.4 MB (54.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false; // No corresponding open bracket
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                    return false; // Mismatched brackets
+                }
+            }
+        }
+
+        return stack.isEmpty(); // Stack should be empty if all brackets are matched.
+    }
+}
