62027adea39eebcf38c98ebebbace0384e60303a
Avinash Kumar <114355501+avikumar0@users.noreply.github.com>
2023-10-15 22:44:51 +0530

    Time: 2 ms (47.81%), Space: 40.9 MB (99.29%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -1,7 +1,34 @@
-import java.util.regex.Pattern;
 class Solution {
     public boolean isNumber(String s) {
-        String regex = "[-+]?(\\d+\\.?\\d*|\\.\\d+)([eE][-+]?\\d+)?";
-        return Pattern.matches(regex, s);
+        if (s == null || s.length() == 0)
+            return false;
+
+        boolean seenDigit = false;
+        boolean seenE = false;
+        boolean seenDot = false;
+
+        s = s.trim();
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                seenDigit = true;
+            } else if (c == 'e' || c == 'E') {
+                if (seenE || !seenDigit || i == n - 1) return false;
+                seenE = true;
+                seenDigit = false;
+            } else if (c == '.') {
+                if (seenDot || seenE) return false;
+                seenDot = true;
+            } else if (c == '+' || c == '-') {
+                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
+            } else {
+                return false;
+            }
+        }
+
+        return seenDigit;
     }
 }
