bea63ec12511974d062fa7a027c3db10776961a5
Avinash Kumar <114355501+avikumar0@users.noreply.github.com>
2023-10-15 22:45:44 +0530

    Time: 1 ms (100%), Space: 41.4 MB (76.12%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -3,27 +3,29 @@ class Solution {
         if (s == null || s.length() == 0)
             return false;
 
+        s = s.trim();
+        int n = s.length();
         boolean seenDigit = false;
         boolean seenE = false;
         boolean seenDot = false;
 
-        s = s.trim();
-        int n = s.length();
-
         for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
 
-            if (Character.isDigit(c)) {
+            if (c >= '0' && c <= '9') {
                 seenDigit = true;
             } else if (c == 'e' || c == 'E') {
-                if (seenE || !seenDigit || i == n - 1) return false;
+                if (seenE || !seenDigit || i == n - 1)
+                    return false;
                 seenE = true;
                 seenDigit = false;
             } else if (c == '.') {
-                if (seenDot || seenE) return false;
+                if (seenDot || seenE)
+                    return false;
                 seenDot = true;
             } else if (c == '+' || c == '-') {
-                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
+                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E')
+                    return false;
             } else {
                 return false;
             }
