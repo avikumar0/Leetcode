e53e1803c4e20a6561aaa1687bb39c70d95f2ff8
Avinash Kumar <114355501+avikumar0@users.noreply.github.com>
2023-10-19 07:21:25 +0530

    Time: 2 ms (100%), Space: 43.4 MB (77.94%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,20 +1,37 @@
 class Solution {
     public int romanToInt(String s) {
-        // Create a HashMap to store the values of Roman numerals.
-        HashMap<Character, Integer> romanValues = new HashMap<>();
-        romanValues.put('I', 1);
-        romanValues.put('V', 5);
-        romanValues.put('X', 10);
-        romanValues.put('L', 50);
-        romanValues.put('C', 100);
-        romanValues.put('D', 500);
-        romanValues.put('M', 1000);
-
         int result = 0;
         int prevValue = 0; // Initialize the previous value to 0.
 
         for (int i = s.length() - 1; i >= 0; i--) {
-            int curValue = romanValues.get(s.charAt(i));
+            char currentSymbol = s.charAt(i);
+            int curValue = 0;
+
+            switch (currentSymbol) {
+                case 'I':
+                    curValue = 1;
+                    break;
+                case 'V':
+                    curValue = 5;
+                    break;
+                case 'X':
+                    curValue = 10;
+                    break;
+                case 'L':
+                    curValue = 50;
+                    break;
+                case 'C':
+                    curValue = 100;
+                    break;
+                case 'D':
+                    curValue = 500;
+                    break;
+                case 'M':
+                    curValue = 1000;
+                    break;
+                default:
+                    break;
+            }
 
             // If the current value is smaller than the previous value, subtract it.
             if (curValue < prevValue) {
