6735aa1298b2f11d37565325843f09a8f7042359
Avinash Kumar <114355501+avikumar0@users.noreply.github.com>
2023-10-25 17:23:23 +0530

    Time: 4 ms (97.29%), Space: 44 MB (21.69%) - LeetHub

---
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int start=0;
+        int end = nums.length-1;
+        int count =0;
+
+        while(start<end){
+
+            count+=nums[end]-nums[start];
+
+            start++;
+            end--;
+        }
+
+        return count;
+    }
+}
