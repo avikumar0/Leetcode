9db3f93bfe8748c7731825aaaf2631383a466c9b
Avinash Kumar <114355501+avikumar0@users.noreply.github.com>
2023-11-06 19:18:54 +0530

    Time: 0 ms (100%), Space: 40.9 MB (34.62%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-
+        
         // Start from the least significant digit
         int carry = 1; // Initialize carry to 1 to add one
         for (int i = n - 1; i >= 0; i--) {
             int sum = digits[i] + carry;
             if (sum >= 10) {
-                digits[i] = sum % 10; // Set the current digit
+                digits[i] = 0; // Set the current digit to 0
                 carry = 1; // Set carry for the next iteration
             } else {
                 digits[i] = sum; // Set the current digit
