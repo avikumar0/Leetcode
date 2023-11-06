d3653f503751dd6af8e7090ae823e0b01e065bcf
Avinash Kumar <114355501+avikumar0@users.noreply.github.com>
2023-11-06 19:18:07 +0530

    Time: 0 ms (100%), Space: 41.1 MB (14.19%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,24 +1,29 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        
+
         // Start from the least significant digit
+        int carry = 1; // Initialize carry to 1 to add one
         for (int i = n - 1; i >= 0; i--) {
-            // Increment the current digit
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits; // No carry, we are done
+            int sum = digits[i] + carry;
+            if (sum >= 10) {
+                digits[i] = sum % 10; // Set the current digit
+                carry = 1; // Set carry for the next iteration
             } else {
-                digits[i] = 0; // Set the current digit to 0 and continue to the next one
+                digits[i] = sum; // Set the current digit
+                carry = 0; // No carry needed
+                break; // Since there's no carry, we are done
             }
         }
-        
-        // If we are here, it means we need to add a new most significant digit (1)
-        int[] result = new int[n + 1];
-        result[0] = 1;
-        
-        // The rest of the digits are already initialized to 0
-        
-        return result;
+
+        // If carry is still 1, it means we need to add a new most significant digit (1)
+        if (carry == 1) {
+            int[] result = new int[n + 1];
+            result[0] = 1;
+            System.arraycopy(digits, 0, result, 1, n); // Copy the remaining digits
+            return result;
+        } else {
+            return digits; // No new most significant digit is needed
+        }
     }
 }
