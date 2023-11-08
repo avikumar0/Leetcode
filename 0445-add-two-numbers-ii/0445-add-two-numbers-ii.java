c1340589f30a9eee9ca79cb249546039836dd6f3
Avinash Kumar <114355501+avikumar0@users.noreply.github.com>
2023-11-08 21:31:36 +0530

    Time: 1 ms (100%), Space: 43.4 MB (52.28%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -10,39 +10,45 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // Create two stacks to store the reversed digits of the input lists
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
+        // Reverse the input linked lists
+        l1 = reverseLinkedList(l1);
+        l2 = reverseLinkedList(l2);
         
-        // Populate the stacks by reversing the linked lists
-        while (l1 != null) {
-            stack1.push(l1.val);
-            l1 = l1.next;
-        }
-        while (l2 != null) {
-            stack2.push(l2.val);
-            l2 = l2.next;
-        }
-        
-        // Initialize variables for sum and carry
-        int sum = 0, carry = 0;
+        int carry = 0;
         ListNode result = null;
         
-        // Process the digits in the stacks and add them with carry
-        while (!stack1.isEmpty() || !stack2.isEmpty() || carry > 0) {
-            int x = stack1.isEmpty() ? 0 : stack1.pop();
-            int y = stack2.isEmpty() ? 0 : stack2.pop();
+        while (l1 != null || l2 != null || carry > 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
             
-            // Calculate the sum for the current position
-            int currentSum = x + y + carry;
-            carry = currentSum / 10;  // Calculate the carry for the next position
+            carry = sum / 10;
+            sum %= 10;
             
-            // Create a new node for the current digit and link it to the result list
-            ListNode newNode = new ListNode(currentSum % 10);
+            ListNode newNode = new ListNode(sum);
             newNode.next = result;
             result = newNode;
         }
         
         return result;
     }
+    
+    private ListNode reverseLinkedList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
 }
+
+
