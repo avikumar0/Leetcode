6a6b60630171d97c8b7d2dd8a0596e1ef5c2b268
Avinash Kumar <114355501+avikumar0@users.noreply.github.com>
2024-08-14 11:18:05 +0530

    Time: 6 ms (35.55%), Space: 41.7 MB (47.88%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,32 +1,22 @@
-class Solution {
+public class Solution {
     public List<String> summaryRanges(int[] nums) {
-        List<String> result = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            return result;
-        }
-        
-        int start = nums[0];
-        
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1] + 1) {
-                // If not consecutive, add the range
-                if (start == nums[i - 1]) {
-                    result.add(String.valueOf(start));
-                } else {
-                    result.add(start + "->" + nums[i - 1]);
-                }
-                // Start a new range
-                start = nums[i];
+        List<String> ans = new ArrayList<>();
+        int n = nums.length;
+        int i = 0;
+
+        while (i < n) {
+            int start = nums[i];
+            while (i < n - 1 && nums[i] + 1 == nums[i + 1]) {
+                i++;
             }
+            if (start != nums[i]) {
+                ans.add(start + "->" + nums[i]);
+            } else {
+                ans.add(String.valueOf(nums[i]));
+            }
+            i++;
         }
-        
-        // Add the final range
-        if (start == nums[nums.length - 1]) {
-            result.add(String.valueOf(start));
-        } else {
-            result.add(start + "->" + nums[nums.length - 1]);
-        }
-        
-        return result;
+
+        return ans;
     }
 }
